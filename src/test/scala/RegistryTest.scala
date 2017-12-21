9d19dd2322a239bb197c800df64ca938124e29a5
Jentsch <daniel.jentsch@leanovate.de>
2017-12-21 11:01:04 +0100

    Added Registry as a later replicated state machine

---
+++ src/test/scala/RegistryTest.scala
@@ -0,0 +1,41 @@
+
+import ClusterTest._
+import akka.typed.testkit.scaladsl.TestProbe
+import org.scalatest.{FlatSpec, Matchers}
+
+class RegistryTest extends FlatSpec with Matchers {
+
+  "registry" should "initially return 0" in cluster { implicit ctx =>
+    val reg = ctx.spawn(Registry.behaviour, "registry")
+    val receiver = TestProbe[Option[Int]]("receiver")
+
+    reg ! Registry.Get('x, receiver.testActor)
+
+    receiver.expectMsg(None)
+  }
+
+  it should "return the last write" in cluster { implicit ctx =>
+    val reg = ctx.spawn(Registry.behaviour, "registry")
+    val receiver = TestProbe[Option[Int]]("receiver")
+
+    reg ! Registry.Set('x, 1, ctx.system.deadLetters)
+    reg ! Registry.Set('x, 2, ctx.system.deadLetters)
+
+    reg ! Registry.Get('x, receiver.testActor)
+
+    receiver.expectMsg(Some(2))
+  }
+
+  it should "return nothing after a delete" in cluster { implicit ctx =>
+    val reg = ctx.spawn(Registry.behaviour, "registry")
+    val receiver = TestProbe[Option[Int]]("receiver")
+
+    reg ! Registry.Set('x, 1, ctx.system.deadLetters)
+    reg ! Registry.Delete('x, ctx.system.deadLetters)
+
+    reg ! Registry.Get('x, receiver.testActor)
+
+    receiver.expectMsg(None)
+  }
+
+}
